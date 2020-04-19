48cca66c863c18bedd54dfc3790f870b24127374
agboola.omowumi <agboola.omowumi@konga.com>
2020-04-19 23:58:17 +0100

    Add form Input

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientsHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+  const removeIngredients = id => {
+    const newIngredients = [...userIngredients].slice(id, 0);
+    setUserIngredients(newIngredients);
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIng={addIngredientsHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredients}
+        />
+        {/* Need to add list here! */}
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
